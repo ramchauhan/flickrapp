13b1301b8b5f23a19b7404808da4185148a0e179
ramchauhan <ramwadhchauhan@gmail.com>
2014-11-02 15:59:02 +0530

    change in admin file

--- flickrphoto/admin.py
+++ flickrphoto/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import UserData
 
-admin.site.register(UserData)
+class UserDataAdmin(admin.ModelAdmin):
+    search_fields = ["search_key"]
+    list_display = ["search_key", "user_ip"]
+
+admin.site.register(UserData, UserDataAdmin)
